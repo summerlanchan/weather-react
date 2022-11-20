a73d820b0af08f2e54dcad7dc5cc99291c1ce0f5
summerlanchan <111342458+summerlanchan@users.noreply.github.com>
2022-11-21 00:34:38 +0900

    Added real openweather data

--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -1,16 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
-export default function WeatherApp() {
+export default function WeatherApp(event) {
+  const [city, setCity] = useState(" ");
+  const [loaded, setLoaded] = useState(false);
+  const [weather, setWeather] = useState("null");
+
+  function displayWeather(response) {
+    setLoaded = true;
+    setWeather({
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      description: response.data.weather[0].description,
+      icon: "http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png",
+    });
+  }
+
   function handleSubmit(event) {
     event.preventDefault();
-    alert("hi");
+    apiUrl =
+      "https://api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKey}";
+    apiKey = "3499ef150985eccadd080ff408a018df";
+    axios.get(apiUrl).then(displayWeather);
   }
-  return (
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  let form = (
     <div>
       <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="Type your city..." />
+        <input
+          type="text"
+          placeholder="Enter your city..."
+          onChange={updateCity}
+        />
         <button type="submit">Search</button>
       </form>
     </div>
   );
+
+  if (loaded === true) {
+    return (
+      <div>
+        {form}
+        <ul>
+          <li>
+            Temperature:{Math.round(response.data.main.temp)} C Humidity:{" "}
+            {weather.humidity}% Description:{weather.description}
+            Wind: {weather.wind} km/h
+            <img src={weather.icon} alt="icon" />
+          </li>
+        </ul>
+      </div>
+    );
+  } else {
+    return form;
+  }
 }
