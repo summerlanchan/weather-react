02bc2282cc6657fe8e705c2fd59a5ed57b3d6987
summerlanchan <111342458+summerlanchan@users.noreply.github.com>
2022-11-21 04:34:59 +0900

    Update WeatherForecast.js

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -8,7 +8,6 @@ export default function WeatherForecast() {
       <div className="card-body weather-days">
         <div className="row row-cols-2">
           <div className="col-2">
-            <h4 className="saturday" />
             SAT <br /> <br />
             &#9728;&#65039;
             <br /> <br />
@@ -16,7 +15,6 @@ export default function WeatherForecast() {
           </div>
 
           <div className="col-2">
-            <h4 className="sunday" />
             SUN <br /> <br />
             &#9728;&#65039;
             <br /> <br />
@@ -24,21 +22,18 @@ export default function WeatherForecast() {
           </div>
           <br />
           <div className="col-2">
-            <h4 className="monday" />
             MON <br /> <br />
             &#9728;&#65039;
             <br /> <br /> <p>35&#8451; 24&#8451;</p>
           </div>
           <br />
           <div className="col-2">
-            <h4 className="tuesday" />
             TUE <br /> <br />
             &#9728;&#65039;
             <br /> <br /> <p>34&#8451; 24&#8451;</p>{" "}
           </div>
           <br />
           <div className="col-2">
-            <h4 className="wednesday" />
             WED <br /> <br />
             &#9728;&#65039;
             <br /> <br /> <p>32&#8451; 25&#8451;</p>{" "}
